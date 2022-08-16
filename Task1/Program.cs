25f522e001022d58931e32f67309c4911d6f283f
Jane <spring1981@mail.ru>
2022-08-16 19:31:35 +0300

    метод подсчета строк в массиве добавлен

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача: 
+// "Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа."
+
+string[] array = { "hello", "2", "world", ":-)", "1234", "1567"};
+
+string PrintArray(string[] array)  
+{
+    return string.Join(",", array);
+}
+
+int CountStringArray(string[] input)
+{
+    int size = array.Length;
+    int count = 0;
+    int maxLengthString = 3;
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i].Length <= maxLengthString)
+        {
+            count++;
+        }
+    }
+    return count;
+}
